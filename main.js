4acdf28c777fd8b940064b4968cdade3d5944f60
Jonathan Carson <carsonjd@colorado.edu>
2017-11-15 18:02:37 -0700

    solutions

--- main.js
+++ main.js
@@ -6,11 +6,13 @@ const example1 = [
 
 //     How would you make Karl's favorite color into a variable in the simplest way possible?
 
-
+// const [{},{},{favColor}] = example1
+// console.log(favColor);
 
 //     How would we a whole object into a variable?
 
-
+// const [obj = {}] = example1
+// console.log(obj)
 
 const example2 = {
   car: {
@@ -30,10 +32,17 @@ const example2 = {
 
 //     How would we use destructuring to create a varible for the car's weight capacity (3652)?
 
-
+// const {car:{weight:[a,b,c]}} = example2
+// console.log(c);
 
 //     How could we use destructuring to calculate mpg?
 
-
+// const {car:{fuelV, dist},mpg} = example2
+// console.log(mpg(dist, fuelV));
 
 //     How would we use destructuring to create a function which returns a string describing who is driving the car?
+
+// function whoDrive ({car:{make, model}, driver:{firstName, lastName}}){
+//   console.log(`${firstName} ${lastName} drives a ${make} ${model}`);
+// }
+// whoDrive(example2)
