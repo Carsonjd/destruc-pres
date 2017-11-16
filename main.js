8208103cb147b444282f90e77fbac11f422227d6
Jonathan Carson <carsonjd@colorado.edu>
2017-11-15 17:59:09 -0700

    initial commit

---
+++ main.js
@@ -0,0 +1,39 @@
+const example1 = [
+  {name: 'Tom', favColor: 'Blue'},
+  {name: 'Ann', favColor: 'Red'},
+  {name: 'Karl', favColor: 'Pink'}
+];
+
+//     How would you make Karl's favorite color into a variable in the simplest way possible?
+
+
+
+//     How would we a whole object into a variable?
+
+
+
+const example2 = {
+  car: {
+    make: 'Geo',
+    model: 'Prizm',
+    weight: [1802, 2130, 3652],
+    fuelV: 10,
+    dist: 400,
+    cool: false
+  },
+  driver: {
+    firstName: 'Bishal',
+    lastName: 'Malla'
+  },
+  mpg: (dist, fuel) => dist/fuel,
+};
+
+//     How would we use destructuring to create a varible for the car's weight capacity (3652)?
+
+
+
+//     How could we use destructuring to calculate mpg?
+
+
+
+//     How would we use destructuring to create a function which returns a string describing who is driving the car?
